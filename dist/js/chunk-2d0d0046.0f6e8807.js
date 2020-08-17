080288af8adc433a006999ed3ac3993d772ce6c3
Michael Nix <mpnix@protonmail.com>
2020-08-17 12:52:08 +0100

    live version for review

---
+++ dist/js/chunk-2d0d0046.0f6e8807.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d0046"],{"65d5":function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("p",[e._v(" News"),t("br"),e._v(" Blog posts"),t("br"),e._v(" Relevant articles ")])}],s=t("2877"),c={},u=Object(s["a"])(c,r,l,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0d0046.0f6e8807.js.map
